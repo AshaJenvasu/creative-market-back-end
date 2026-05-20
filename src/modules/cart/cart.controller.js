88b72476f6a23f3364f8e8934d9217465a92403f
AshaJenvasu <chaiyawatjenvasu@gmail.com>
2026-05-20 14:36:54 +0700

    Update Cart schema with addtocartitem and clear the cart

---
+++ src/modules/cart/cart.controller.js
@@ -0,0 +1,39 @@
+import { Cart } from "./cart.model.js";
+
+export const addItemToCart = async (req, res, next) => {
+  try {
+    const { userId, productId, quantity } = req.body;
+    let cart = await Cart.findOne({ userId });
+
+    if (cart) {
+      // ถ้าตะกร้ามีแล้ว เช็คว่าสินค้าตัวนี้มีในตะกร้าหรือยัง
+      const itemIndex = cart.items.findIndex((p) => p.productId == productId);
+      if (itemIndex > -1) {
+        cart.items[itemIndex].quantity += quantity;
+      } else {
+        cart.items.push({ productId, quantity });
+      }
+      await cart.save();
+    } else {
+      // ถ้าตะกร้ายังไม่มี ให้สร้างใหม่
+      cart = await Cart.create({
+        userId,
+        items: [{ productId, quantity }],
+      });
+    }
+    res.status(200).json({ success: true, data: cart });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// ล้างตะกร้าของ User คนนั้นๆ
+export const clearCart = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    await Cart.findOneAndDelete({ userId });
+    res.status(200).json({ success: true, message: "Cart cleared" });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { router as productsRoutes } from "./product.route.js";
+import { router as cartRoutes } from "./cart.route.js";
 
 export const router = Router();
 
 router.use("/products", productsRoutes);
+router.use("/cart", cartRoutes);
